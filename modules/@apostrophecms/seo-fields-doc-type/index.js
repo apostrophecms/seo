926339023cf55a9c74b4f401e6cea82125a5734f
Anthony Tarlao <anthony.tarlao@gmail.com>
2023-03-08 19:17:08 +0100

    Canonical pieces

--- modules/@apostrophecms/seo-fields-doc-type/index.js
+++ modules/@apostrophecms/seo-fields-doc-type/index.js
@@ -1,8 +1,10 @@
+const _ = require('lodash');
+
 module.exports = {
   improve: '@apostrophecms/doc-type',
   fields(self, options) {
     if (options.seoFields !== false) {
-      return {
+      const configuration = {
         add: {
           seoTitle: {
             label: 'aposSeo:title',
@@ -42,6 +44,43 @@ module.exports = {
           }
         }
       };
+
+      if (self.options.seoCanonicalTypes
+        && Array.isArray(self.options.seoCanonicalTypes)
+        && self.options.seoCanonicalTypes.length) {
+
+        const req = options.apos.task.getReq();
+        self.options.seoCanonicalTypes.forEach(canonicalType => {
+          const name = canonicalType.startsWith('@apostrophecms/') ? canonicalType.split('@apostrophecms/')[1] : '';
+          const fieldName = canonicalType.startsWith('@apostrophecms/')
+            ? `_${_.kebabCase(name)}`
+            : `_seoCanonical${canonicalType}`;
+          const moduleName = options.apos.modules[canonicalType]
+            ? options.apos.modules[canonicalType].label
+            : name.replace(/-/, ' ');
+          const label = req.t('aposSeo:canonicalModule', { type: _.startCase(req.t(moduleName) || canonicalType) });
+          const help = req.t('aposSeo:canonicalHelpModule', { type: req.t(moduleName) || canonicalType });
+
+          configuration.add[fieldName] = {
+            help,
+            label,
+            max: 1,
+            type: 'relationship',
+            withType: canonicalType,
+            builders: {
+              project: {
+                title: 1,
+                slug: 1,
+                _url: 1
+              }
+            }
+          };
+
+          configuration.group.seo.fields.push(fieldName);
+        });
+      }
+
+      return configuration;
     }
   }
 };
